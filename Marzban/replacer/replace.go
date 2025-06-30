9f751dd8bb1d20228b2ed7ac4a5776d26ea807f9
Pourya Samimi <neverdiegenius@gmail.com>
2025-06-30 21:10:55 +0000

    feat: Create replace.go to replace .env and xray_config.json and run xray core with new congfig

--- Marzban/replacer/replace.go
+++ Marzban/replacer/replace.go
@@ -1,1 +1,54 @@
 package replacer
+
+import (
+	"io"
+	"os"
+)
+
+func Replace_xray() error {
+	dstPath := "/var/lib/marzban/xray_config.json"
+	srcPath := "xray_config.json"
+
+	src, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Open(dstPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Replace_env() error {
+	dstPath := "/opt/marzban/.env"
+	srcPath := ".env"
+
+	src, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Open(dstPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
