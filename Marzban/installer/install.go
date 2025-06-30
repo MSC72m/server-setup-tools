89d0e9278c0b914012423dcdec39c75fe1385fe3
Pourya Samimi <neverdiegenius@gmail.com>
2025-06-30 20:36:00 +0000

    feat: Create install.go for start marzban panel installation with bach commend

--- Marzban/installer/install.go
+++ Marzban/installer/install.go
@@ -1,1 +1,22 @@
 package installer
+
+import (
+	"context"
+	"errors"
+	"os/exec"
+	"time"
+)
+
+func Install_Marzban() error {
+	ctx, cencel := context.WithTimeout(context.Background(), 2 * time.Minute)
+	defer cencel()
+
+	cmd := exec.CommandContext(ctx, "sudo", "bash", "-c", `$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh) @ install`)
+	
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return errors.New("marzban install error") 
+	}
+
+	return nil
+}
